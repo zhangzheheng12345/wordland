b407c5d654bb9d49984b3ddf2a08642926ce2401
zhangzheheng12345 <76390451+zhangzheheng12345@users.noreply.github.com>
2023-05-07 10:04:22 +0800

    Update and fix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '文字岛';
+export const SITE_DESCRIPTION = '文字的岛屿 在浪中静止';
