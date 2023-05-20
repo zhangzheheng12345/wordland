c53a2f99f5b38b940b5c01b1f88f5c69eafae3ca
zhangzheheng12345 <76390451+zhangzheheng12345@users.noreply.github.com>
2023-05-20 09:20:16 +0800

    Fix and update styles

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,9 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "文字岛 | Wordland 🏝️";
-export const SITE_DESCRIPTION = "在繁华的世界里 找寻文字的痕迹";
+export const SITE_TITLE = '文字岛 | Wordland 🏝️';
+export const SITE_DESCRIPTION = '在繁华的世界里 找寻文字的痕迹';
 
 export const AUTHORS = {
-  "Youzuo Zhang": {
-    cnName: "张又左",
-    email: "zhangzheheng12345@163.com",
+  'Youzuo Zhang': {
+    cnName: '张又左',
+    email: 'zhangzheheng12345@163.com',
   },
 };
