97b66517131c94104fa098d632172a9f91c91195
zhangzheheng12345 <76390451+zhangzheheng12345@users.noreply.github.com>
2023-06-03 10:31:51 +0800

    Update

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 网站：[https://wordland.netlify.app](https://wordland.netlify.app)
 
-随心而写。
+随心而写，随心而阅。
 
 投稿：_zhangzheheng12345@163.com_
--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,12 @@ export const SITE_DESCRIPTION = '在繁华的世界里 找寻文字的痕迹'
 export const AUTHORS = {
   'Youzuo Zhang': {
     cnName: '张又左',
-    badges: ['编辑 EDTITOR', '撰写人 AUTHOR'],
+    badges: ['编辑', '撰写人'],
     email: 'zhangzheheng12345@163.com'
   },
   'Yanru Chen': {
     cnName: '陈晏如',
-    badges: ['撰写人 AUTHOR'],
+    badges: ['撰写人'],
     email: 'WAIT TO COME'
   }
 }
