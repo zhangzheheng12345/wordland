e85d060ee0b8a09adb0ad013b05df87d80fa6874
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2023-06-18 20:20:09 +0800

    Refactor CSS and brand-new style update

--- src/components/Like.vue
+++ src/components/Like.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="flex items-center">
+  <div
+    class="flex items-center cursor-default animate-iteration-1 animate-bounce-alt active:animate-none throttle"
+    @click="toggle"
+    :style="`animation-duration:${duration}s;`"
+  >
     <span
       class="m-3px"
       :class="toLike ? 'i-tabler-thumb-up-filled' : 'i-tabler-thumb-up'"
     ></span>
-    <span @click="toggle" class="font-550">{{ likesCount }}</span>
+    <span class="font-550">{{ likesCount }}</span>
   </div>
 </template>
 
@@ -24,9 +28,14 @@ const props = defineProps<{
 const toLike = useLocalStorage(getStorageKeyForArticleLikes(props.name), false)
 const likesCount = useLocalStorage(getStorageKeyToLike(props.name), 0)
 
+const duration = ref(0)
+
 onMounted(async () => {
   const res = await getArticleLikes(props.name)
   if (res) likesCount.value = res as number
+  setTimeout(() => {
+    duration.value = 1
+  }, 1000)
 })
 
 function toggle() {
